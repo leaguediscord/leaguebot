6ad6f7454f47d1a60871df8f86a9ce37182fda4b
anisiosts <anisio.dev@gmail.com>
2019-07-12 14:52:29 -0300

    [feature] adicionando comando help

---
+++ app/help/commands.js
@@ -0,0 +1,37 @@
+
+module.exports = {
+    run: (client, message, args) => {
+
+        let embed = {
+        color: 0x993399,
+        title: 'Lista de Comandos LeagueBot',
+        url: 'https://github.com/Liga-dos-Programadores/Project-A',
+        description: 'Todos os comandos disponíveis',
+        fields: []
+      }
+
+      client.commands.forEach(command => {
+        if (command.alias) return
+        embed.fields.push(
+          {
+            name: `**${process.env.PREFIX}${command.help.name}**`,
+            value: `**Descrição**: ${command.help.description}\n**Como Usar**: ${process.env.PREFIX} ${command.help.usage}`
+          }
+        )
+      })
+  
+      message.author.send({ embed: embed })
+        .then(() => message.react('👌'))
+        .catch(() => message.reply('Desculpe, mas eu não tenho permissões para enviar mensagens por DM para você!'))
+    },
+  
+    conf: {},
+  
+    help: {
+      name: 'help',
+      category: 'Help',
+      description: 'Mostra todos os comandos disponíveis do bot.',
+      usage: 'help'
+    }
+  }
+  
